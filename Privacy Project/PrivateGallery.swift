a748ea3e74939a025dd6de96624272b71c1a53b1
Timothy Dai <ttd004@emai>
2019-10-31 16:33:09 -0500

    Created new PrivateGallery swift file and linked

---
+++ Privacy Project/PrivateGallery.swift
@@ -0,0 +1,21 @@
+//
+//  PrivateGallery.swift
+//  Privacy Project
+//
+//  Created by Timothy Dai on 10/31/19.
+//  Copyright © 2019 Capstone. All rights reserved.
+//
+
+import UIKit
+
+class PrivateGallery: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func onGoToPrivacyProject(_ sender: Any) {
+    }
+    
+}
